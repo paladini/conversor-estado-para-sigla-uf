fd49c2df3ffb494c4448de89abb01edcf82758ce
Beta Faccion <betafcc@gmail.com>
2017-12-07 15:58:27 -0200

    Adds bidirectional update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const {join} = require('path');
 const camelCase = require('lodash.camelcase');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const package = require('./package.json');
+const {PORT=3030, NODE_ENV='production'} = process.env;
 
-module.exports = {
+
+const base = {
   entry: {
     index: join(__dirname, 'src')
   },
@@ -14,14 +17,6 @@ module.exports = {
     library: camelCase(package.name.split('/').slice(-1)[0]),
     libraryTarget: 'umd',
     path: join(__dirname, 'dist'),
-  },
-  externals: {
-    'fuse.js': {
-      commonjs: 'fuse.js',
-      commonjs2: 'fuse.js',
-      amd: 'fuse.js',
-      root: 'Fuse',
-    },
   },
   resolve: {
     extensions: ['.js', '.json']
@@ -35,7 +30,49 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-  ]
 };
+
+
+const env = {
+  development: {
+    devtool: 'source-map',
+    devServer: {
+      hot: true,
+      inline: true,
+      historyApiFallback: true,
+      port: PORT,
+      publicPath: '/',
+      contentBase: join(__dirname, '.'),
+      watchContentBase: true,
+      stats: {
+        colors: true
+      }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: join(__dirname, 'index.html'),
+        inject: 'head',
+      })
+    ]
+  },
+
+  production: {
+    externals: {
+      'fuse.js': {
+        commonjs: 'fuse.js',
+        commonjs2: 'fuse.js',
+        amd: 'fuse.js',
+        root: 'Fuse',
+      },
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin(),
+    ]
+  }
+};
+
+
+module.exports = Object.assign({}, base, env[NODE_ENV]);
